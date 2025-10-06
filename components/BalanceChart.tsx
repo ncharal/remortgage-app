de653472b4e92a1e0d113a262d97ac080d5e99bd
ncharal <nicompk@googlemail.com>
2025-10-06 15:45:21 +0100

    Fixed Chart React Type Clash

--- components/BalanceChart.tsx
+++ components/BalanceChart.tsx
@@ -1,19 +1,16 @@
 "use client";
 
 import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  Tooltip as RTooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import * as Recharts from "recharts";
 
-// Recharts sometimes bundles its own @types/react which conflicts with the app's.
-// Casting to any here sidesteps the conflicting ReactNode types at compile time.
-const RC = ResponsiveContainer as unknown as React.ComponentType<any>;
+// Pull components then cast them to avoid @types/react mismatches
+const RC = Recharts.ResponsiveContainer as unknown as React.ComponentType<any>;
+const LC = Recharts.LineChart as unknown as React.ComponentType<any>;
+const LN = Recharts.Line as unknown as React.ComponentType<any>;
+const XAx = Recharts.XAxis as unknown as React.ComponentType<any>;
+const YAx = Recharts.YAxis as unknown as React.ComponentType<any>;
+const TT = Recharts.Tooltip as unknown as React.ComponentType<any>;
+const LG = Recharts.Legend as unknown as React.ComponentType<any>;
 
 export default function BalanceChart({
   data,
@@ -25,16 +22,20 @@ export default function BalanceChart({
   return (
     <div className="h-72 w-full">
       <RC width="100%" height="100%">
-        <LineChart data={data} margin={{ top: 10, right: 20, bottom: 0, left: 0 }}>
-          <XAxis dataKey="month" tickFormatter={(v: number) => `${v}m`} />
-          <YAxis tickFormatter={(v: number) => `£${(v / 1000).toFixed(0)}k`} />
-          {/* @ts-expect-error – Recharts tooltip types can conflict in Next 14 */}
-          <RTooltip formatter={(v: any) => `£${Number(v).toFixed(2)}`} labelFormatter={(l: any) => `Month ${l}`} />
-          <Legend />
+        <LC data={data} margin={{ top: 10, right: 20, bottom: 0, left: 0 }}>
+          <XAx dataKey="month" tickFormatter={(v: number) => `${v}m`} />
+          <YAx tickFormatter={(v: number) => `£${(v / 1000).toFixed(0)}k`} />
+          {/* Recharts’ Tooltip types can be incompatible in some setups */}
+          {/* @ts-expect-error */}
+          <TT
+            formatter={(v: any) => `£${Number(v).toFixed(2)}`}
+            labelFormatter={(l: any) => `Month ${l}`}
+          />
+          <LG />
           {seriesNames.map((name, idx) => (
-            <Line key={idx} type="monotone" dataKey={name} dot={false} strokeWidth={2} />
+            <LN key={idx} type="monotone" dataKey={name} dot={false} strokeWidth={2} />
           ))}
-        </LineChart>
+        </LC>
       </RC>
     </div>
   );
